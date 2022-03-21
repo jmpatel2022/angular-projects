8960ef6134a8e6b4dcc565e163466869d642650f
jmpatel2022 <jyotikampatel@gmail.com>
2022-03-20 22:29:06 -0400

    articles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticlesComponent } from './articles/articles/articles.component';
 import { ContactComponent } from './contact/contact/contact.component';
 
 const routes: Routes = [
-  { path: 'contact', component: ContactComponent }
+  { path: 'contact', component: ContactComponent },
+  { path: 'articles', component: ArticlesComponent}
 ];
 
 @NgModule({
